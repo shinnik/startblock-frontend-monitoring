106fa4b369dd0e2d521969da1d66b70c9c24426b
Nikita Izrailev <izrailev@phystech.edu>
2019-07-13 15:09:52 +0300

    first version of plot

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import {ThemeProvider} from "@material-ui/styles";
 import theme from './styles/theme';
 import Main from "./containers/Main/Main";
+import PlotContainer from "./containers/PlotContainer/PlotContainer";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-        <Main />
+        <Main/>
+        <PlotContainer/>
     </ThemeProvider>
   );
 }

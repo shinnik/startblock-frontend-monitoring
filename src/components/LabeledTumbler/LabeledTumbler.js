8e8d994979656f63609ff61effbe7372d6fed56b
Kyzyl-ool <kyzyloolk@mail.ru>
2019-07-13 16:15:20 +0300

    Additional text on hover

--- src/components/LabeledTumbler/LabeledTumbler.js
+++ src/components/LabeledTumbler/LabeledTumbler.js
@@ -2,17 +2,29 @@ import React, {useState} from 'react';
 import Tumbler from "../Tumbler/Tumbler";
 import './LabeledTumbler.scss';
 import {Box, Typography} from "@material-ui/core";
+import {watt} from "../../constants/names";
+
 
 function LabeledTumbler({direction, power, label, type, align, id, dispatch, koeff}) {
     const [state, setState] = useState(power);
+    const [labelState, setLabelState] = useState(label);
 
     return (
         <div className={`LabeledTumbler LabeledTumbler__${direction}`}>
-            <Box className={`Arrow__${type}`} >
+            <Box onMouseOut={() => {
+                setLabelState(label);
+            }}
+                 onMouseEnter={() => {
+                     setLabelState(`${label} ${watt}`);
+                 }}
+                 onMouseMoveCapture={() => {
+                     setLabelState(`${label} ${watt}`);
+                 }}
+                 className={`Arrow__${type}`} >
                 <Tumbler direction={direction} power={state} id={id} dispatch={dispatch} hell={setState} koeff={koeff}/>
             </Box>
-            <Box className={`Label__${type} Align_${align}`}>
-                { state && <Typography variant='body2' color='secondary'>{`${label}`}</Typography> }
+            <Box hidden={!state} className={`Label__${type} Align_${align}`}>
+                <Typography variant='body2' color='secondary'>{`${labelState}`}</Typography>
             </Box>
         </div>
     );

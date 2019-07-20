a69e8ecf58fc0a2e401c0954c891c2f1964ab658
Kyzyl-ool <kyzyloolk@mail.ru>
2019-07-20 18:42:04 +0300

    responsive header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,10 +3,13 @@ import Icon from "../Icon/Icon";
 import ButtonsGroup from "../ButtonsGroup/ButtonsGroup";
 
 import styles from './Header.module.scss';
+import useWindowSize from "@rehooks/window-size";
 
 const Header = ({ setMode, mode }) => {
+    const windowSize = useWindowSize();
+
     return (
-        <div className={styles['header__container']}>
+        <div style={{zoom: windowSize.innerWidth/1200}} className={styles['header__container']}>
             <Icon/>
             <h1 className={styles['header__label']}>Мониторинг</h1>
             <ButtonsGroup setMode={setMode} mode={mode}/>

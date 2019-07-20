b486eca9265653e6e7b431c1b1996f7b3986b2b3
Kyzyl-ool <kyzyloolk@mail.ru>
2019-07-20 19:03:51 +0300

    trashin

--- src/App.js
+++ src/App.js
@@ -7,29 +7,17 @@ import classes from './App.module.scss';
 import HeaderContainer from "./containers/HeaderContainer/HeaderContainer";
 import { w3cwebsocket as W3CWebSocket } from "websocket";
 
-const client = new W3CWebSocket('wss://onder2.herokuapp.com');
 
 function App() {
-    useEffect(() => {
-       client.onopen = () => {
-           console.log('WebSocket Client Connected');
-       };
-    }, []);
-    useEffect(() => {
-        client.onmessage = (message) => {
-            console.log(message);
-        };
-    });
-
-  return (
-    <ThemeProvider theme={theme}>
-        <div className={classes.App}>
-            <HeaderContainer/>
-            <Main/>
-            <PlotContainer/>
-        </div>
-    </ThemeProvider>
-  );
+    return (
+      <ThemeProvider theme={theme}>
+          <div className={classes.App}>
+              <HeaderContainer/>
+              <Main/>
+              <PlotContainer/>
+          </div>
+      </ThemeProvider>
+    );
 }
 
 export default App;

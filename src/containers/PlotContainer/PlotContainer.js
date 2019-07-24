260c7b05476a9a476d081362c02a49d759c6c151
Nikita Izrailev <izrailev@phystech.edu>
2019-07-24 21:38:10 +0300

    added first attemtpt to get data

--- src/containers/PlotContainer/PlotContainer.js
+++ src/containers/PlotContainer/PlotContainer.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import Plot from "../../components/Plot/Plot";
-import generateMock from "../../helpers/generateMockPlotData";
+// import generateMock from "../../helpers/generateMockPlotData";
 import { w3cwebsocket as W3CWebSocket } from 'websocket';
+import PlotRequestModel from '../../models/Plot/plot';
 
-const client = new W3CWebSocket('wss://Onder2.herokuapp.com');
+const client = new W3CWebSocket('wss://Onder2.herokuapp.com/plot');
 
 const PlotContainer = () => {
 
@@ -12,13 +13,17 @@ const PlotContainer = () => {
   const [internet, setInternet] = useState({});
 
   useEffect(() => {
+    PlotRequestModel.getInitialPoints().then(data => console.log('GET RESPONSE', data));
+  }, []);
+
+  useEffect(() => {
     client.onopen = () => {
       console.log('WebSocket Client Connected');
     };
     client.onmessage = (message) => {
-      // setState
+      console.log('WEBSOCKET MESSAGE', message);
     };
-  }, [])
+  }, []);
   return (
     <Plot traditionalData={traditional}
           distributionData={distributed}

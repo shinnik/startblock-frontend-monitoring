5c9db8a57a79416062702a5295155d8f85e318ea
Kyzyl-ool <kyzyloolk@mail.ru>
2019-07-30 02:44:18 +0300

    reverting

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,13 +3,10 @@ import Icon from "../Icon/Icon";
 import ButtonsGroup from "../ButtonsGroup/ButtonsGroup";
 
 import styles from './Header.module.scss';
-import useWindowSize from "@rehooks/window-size";
 
 const Header = ({ setMode, mode }) => {
-    const windowSize = useWindowSize();
-
     return (
-        <div style={{zoom: windowSize.innerWidth/1200}} className={styles['header__container']}>
+        <div className={styles['header__container']}>
             <Icon/>
             <h1 className={styles['header__label']}>Мониторинг</h1>
             <ButtonsGroup setMode={setMode} mode={mode}/>

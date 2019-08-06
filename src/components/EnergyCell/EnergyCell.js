780fe988d9b21409f3dbb690ac197558f1f5a325
Kyzyl-ool <kyzyloolk@mail.ru>
2019-08-06 17:27:28 +0300

    padding fix

--- src/components/EnergyCell/EnergyCell.js
+++ src/components/EnergyCell/EnergyCell.js
@@ -8,7 +8,7 @@ function EnergyCell({name, money, active, ...rest}) {
     return <Paper elevation={active ? 1 : 2} {...rest}>
         <Typography style={{display: active ? 'inline' : 'none', fontSize: '32px', paddingTop: '8px', paddingLeft: '10px'}}><b>{name}</b></Typography>
             <Box style={{display: active ? 'inline' : 'none',}} className={classes.Box}>
-                <Box style={{paddingBottom: '8px'}}>
+                <Box style={{paddingBottom: '8px', paddingLeft: '8px'}}>
                     <Typography style={{fontFamily: 'Roboto Mono'}} variant='body2'>{`${money}`}</Typography>
                     <Typography variant={'body2'}>{`${currency}`}</Typography>
                 </Box>

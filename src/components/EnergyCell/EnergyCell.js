82f138173fd2977dae690c9afa325496d9ebecce
Kyzyl-ool <kyzyloolk@mail.ru>
2019-08-07 14:53:44 +0300

    block div

--- src/components/EnergyCell/EnergyCell.js
+++ src/components/EnergyCell/EnergyCell.js
@@ -6,8 +6,8 @@ import classes from './EnergyCell.module.scss';
 
 function EnergyCell({name, money, active, ...rest}) {
     return <Paper elevation={active ? 1 : 2} {...rest}>
-        <Typography style={{display: active ? 'inline' : 'none', fontSize: '32px', paddingTop: '8px', paddingLeft: '10px'}}><b>{name}</b></Typography>
-            <Box style={{display: active ? 'inline' : 'none',}} className={classes.Box}>
+        <Typography style={{display: active ? 'block' : 'none', fontSize: '32px', paddingTop: '8px', paddingLeft: '10px'}}><b>{name}</b></Typography>
+            <Box style={{display: active ? 'block' : 'none',}} className={classes.Box}>
                 <Box style={{paddingBottom: '8px', paddingLeft: '8px'}}>
                     <Typography style={{fontFamily: 'Roboto Mono'}} variant='body2'>{`${money}`}</Typography>
                     <Typography variant={'body2'}>{`${currency}`}</Typography>

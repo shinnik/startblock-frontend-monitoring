7c0adde316f988f60c836000616dabd1f8826301
Kyzyl-ool <kyzyloolk@mail.ru>
2019-10-08 15:20:12 +0300

    Update endpoints.js

--- src/constants/endpoints.js
+++ src/constants/endpoints.js
@@ -1,4 +1,4 @@
-export const BACKEND_IP = '35.234.105.87';
+export const BACKEND_IP = '35.234.105.87:443';
 export const WEBSOCKET_URIS = ['cells', 'arrows', 'arrowdirections', 'router', 'agents'];
 export const PLOT_DATA = `http://${BACKEND_IP}/data`;
 export const WEBSOCKET_SERVER = `ws://${BACKEND_IP}`;
